c675298e00e56afa84ba634cc2bfe1042ca69bfb
EDA <eda@mac-mini-6.local>
2016-10-12 11:18:12 +1300

    changed unit case in WeatherToday.jsx for consistency

--- src/services/weatherservice.js
+++ src/services/weatherservice.js
@@ -2,9 +2,9 @@ var Fetch = require('whatwg-fetch');
 var baseUrl = 'http://api.openweathermap.org/data/2.5/forecast?q=';
 
 // Test the API directly in the frontend
-var apiKey = 'yourApiKey';
+var apiKey = '604ad351b4541e4025b3f0034158d604';
 
-// Use this you're ready to use as environment variable
+// // Use this you're ready to use as environment variable
 // var apiKey = process.env.OPENWEATHER_API_KEY;
 
 var weatherService = {
